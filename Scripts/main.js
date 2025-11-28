088f2b15e567b031aa94e2257bbdde8776481a0b
Everton Alencar <evertonalencar2k20@gmail.com>
2025-11-28 11:53:53 -0300

    added dropdown menu

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,12 @@
 function tooglebtn (){
     let btn = document.getElementById("toast-menu-button");
+    let dropdowMenu = document.getElementById("dropdown-menu");
     if (btn.innerHTML == '<i class="fa-solid fa-bars"></i>') {
-        btn.innerHTML = '<i class="fa-solid fa-x"></i>'
+        dropdowMenu.style.display = "flex";
+        dropdowMenu.style.animation = "pulse 1s";
+        btn.innerHTML = '<i class="fa-solid fa-x"></i>';
     } else if (btn.innerHTML == '<i class="fa-solid fa-x"></i>') {
-        btn.innerHTML = '<i class="fa-solid fa-bars"></i>'
+        dropdowMenu.style.display = "none";
+        btn.innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 }
